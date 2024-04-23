8fa75d0138968ce190590a8531f64669047e0c0a
Yuva sai Varma Jampana <yuvasaivarmaj@gmail.com>
2024-04-23 11:55:06 -0400

    final draft

--- src/main/java/com/Tempt/Hunger/Temptations/Config/SecurityConfig.java
+++ src/main/java/com/Tempt/Hunger/Temptations/Config/SecurityConfig.java
@@ -1,35 +1,47 @@
 package com.Tempt.Hunger.Temptations.Config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
+    @Autowired
+    DataSource dataSource;
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("user")
-                .password(passwordEncoder().encode("yuv"))
-                .roles("USER")
-                .and()
-                .withUser("blah")
-                .password(passwordEncoder().encode("123"))
-                .authorities("ROLE_ADMIN");
+        auth.jdbcAuthentication()
+        .dataSource(dataSource)
+
+                .withDefaultSchema()
+                .withUser(
+                        User.withUsername("user")
+                                .password(passwordEncoder().encode("pass"))
+                                .roles("USER")
+                )
+                .withUser(
+                        User.withUsername("admin")
+                                .password(passwordEncoder().encode("pass"))
+                                .roles("ADMIN")
+                );
+
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/user").hasRole("USER")
-                .antMatchers("/**").hasAuthority("ROLE_ADMIN")
+                .antMatchers("/user").hasAnyRole("USER","ADMIN")
+                .antMatchers("/admin").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
